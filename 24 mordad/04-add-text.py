9187dfa3762b43854512da16460632979edeb909
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-08-15 18:00:51 +0330

    asdf

--- 24 mordad/04-add-text.py
+++ 24 mordad/04-add-text.py
@@ -11,12 +11,19 @@ def add_text(a):
     global text
     text = text + a
     l1.config(text=text)
+def mosavi():
+    global text
+    print(eval(text))
 
 l1 = tk.Label(window, text=text, padx=10, pady=10)
 btn1 = tk.Button(window, text='1', padx=10, pady=10, command=lambda: add_text('1'))
 btn2 = tk.Button(window, text='0', padx=10, pady=10, command=lambda: add_text('0'))
+btn4 = tk.Button(window, text='+', padx=10, pady=10, command=lambda: add_text('+'))
+btn3 = tk.Button(window, text='=', padx=10, pady=10, command=mosavi)
 l1.pack()
 btn1.pack()
 btn2.pack()
+btn4.pack()
+btn3.pack()
 
 window.mainloop()
