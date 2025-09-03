90f3d69d0daa2dd95bdbe6e10d63069d45d0785f
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-09-03 19:44:01 +0330

    my awsome commit

--- 12 Shahrivar/03-start-github.py
+++ 12 Shahrivar/03-start-github.py
@@ -1,15 +1,18 @@
 import pyautogui as pg
+import keyboard
 import time
 
-pg.moveTo(954, 1054, duration=1)
-pg.click()
-pg.moveTo(206, 789, duration=3)
-pg.click()
-pg.write('my awsome commit')
-pg.moveTo(252, 996, duration=1)
-pg.click()
-time.sleep(2)
-pg.hotkey('ctrl', 'p')
 
 while True:
-    print(pg.position())
+    if keyboard.is_pressed('x'):
+        pg.moveTo(954, 1054, duration=1)
+        pg.click()
+        pg.moveTo(206, 789, duration=3)
+        pg.click()
+        pg.write('my awsome commit')
+        pg.moveTo(252, 996, duration=1)
+        pg.click()
+        time.sleep(2)
+        pg.hotkey('ctrl', 'p')
+    if keyboard.is_pressed('z'):
+        break
